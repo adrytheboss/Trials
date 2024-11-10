a1e22655ac0b4683b4cde8dae45e6d034870d563
adrytheboss <adrianmerinogarcia@gmail.com>
2024-11-10 19:42:52 +0100

    Fucion de suma de python mas la modificaion del README.md

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Trials
-commit/ pull/ push/ rebase/ multiple branching/.....
+This is a repo to review everything from lecture modules from 1 to 3
---
+++ plotting.py
@@ -0,0 +1,12 @@
+def suma(a,b):
+	return a + b 
+
+def main(): 
+	a = input()
+	b = input()
+	sum = suma(a,b)
+	print(f'La suma de {a} + {b} es igual a {sum}')
+	return
+
+if __name__ == "__main__": 
+	main()
